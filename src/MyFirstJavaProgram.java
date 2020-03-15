c143c565e6b88097f154584992b73bb6eba996b6
Shruti Dixit <shrutidixit@msgais-MacBook-Pro-6.local>
2020-03-15 18:48:46 +0530

    This is my first program

---
+++ src/MyFirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class MyFirstJavaProgram {
+    /* This is my java program.
+    hjhjg
+     */
+
+    public static void main(String []args){
+        System.out.println("hello world");
+    }
+}
