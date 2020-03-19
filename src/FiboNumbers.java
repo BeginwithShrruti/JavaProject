fd0d1adf4226d4ce7c6dff86d7df13bca60afa7e
beginwithshruti <lang4shruti@gmail.com>
2020-03-20 00:00:48 +0530

    latest update

--- src/FiboNumbers.java
+++ src/FiboNumbers.java
@@ -2,10 +2,10 @@ public class FiboNumbers {
 
     public static void main(String []args)
     {
-        int a=1, b=2, c=3, Z, i, count=10;
-        System.out.println(a+" "+b); //printing 1 & 2
+        int a=1, b=2, c=3, Z, i, count=20;
+        System.out.println(a+" "+b+" "+c); //printing 1 & 2 & 3
 
-        for(i=4; i<count; i++); //1 & 2 are printing already, so the loop will start from 2
+        for(i=4; i<count; i++)
         {
             Z = a+b+c;
             System.out.println(" "+Z);
