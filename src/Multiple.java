c8edd35de6312a209f2ac17b86a6cd99a802f2ac
beginwithshruti <lang4shruti@gmail.com>
2020-03-21 00:16:36 +0530

    multiplication program of 2 numbers

---
+++ src/Multiple.java
@@ -0,0 +1,17 @@
+public class Multiple {
+    public static void main (String[] args)
+    {
+
+        int result = numMulti(2, 5);
+        System.out.println(result);
+    }
+
+
+    //this is shruti
+    public static int numMulti(int n1, int n2)
+    {
+        int result = n1 * n2;
+        return result;
+    }
+}
+
